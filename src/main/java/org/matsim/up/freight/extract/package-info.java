e9aeeea0e2e1ab680daf987ffd08a57678ae659b
Johan W. Joubert <johan.joubert@up.ac.za>
2021-03-04 10:16:40 +0200

    Added package infor to restart build
    While the build passed on Travis, the deployment failed.

---
+++ src/main/java/org/matsim/up/freight/extract/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * The code in this package forms a workflow to extract commercial vehicle
+ * activity chains from the raw GPS trace data provided by Digicore Holdings.
+ */
+package org.matsim.up.freight.extract;
